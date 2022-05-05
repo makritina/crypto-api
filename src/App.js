b87fa310df1e825e4be38b98da556818311d1e27
Tina Makri <makritina19@gmail.com>
2022-05-05 18:42:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import CurrencyConv from "./components/CurrencyConv";
+import NewsFeed from "./components/NewsFeed";
+import ExchangeRate from "./components/ExchangeRate";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <CurrencyConv />
+      <NewsFeed />
     </div>
   );
-}
+};
 
 export default App;
